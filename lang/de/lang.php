2720c54eb452537abbe8b00fe23bd83aebafcac1
Tim Weinhold <tim.weinhold@gmail.com>
2013-08-15 11:15:15 +0200

    translation update

---
+++ lang/de/lang.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Tim Weinhold <tim.weinhold@gmail.com>
+ */
+$lang['menu']                  = 'Zugriffs- und Verwendungsstatistik';
+$lang['more']                  = 'mehr';
+$lang['prev']                  = 'Vorherige Seite';
+$lang['next']                  = 'Nächste Seite';
+$lang['time_select']           = 'Zeitfenster auswählen:';
+$lang['time_today']            = 'Heute';
+$lang['time_last1']            = 'Gestern';
+$lang['time_last7']            = 'Letzte 7 Tage';
+$lang['time_last30']           = 'Letzte 30 Tage';
+$lang['time_go']               = 'Los';
+$lang['dashboard']             = 'Dashboard';
+$lang['page']                  = 'Seiten';
+$lang['referer']               = 'Eingehende Links';
+$lang['newreferer']            = 'Neue eingehende Links';
+$lang['outlinks']              = 'Ausgehende Links';
+$lang['searchphrases']         = 'Externe Suchausdrücke';
+$lang['searchwords']           = 'Externe Suchwörter';
+$lang['internalsearchphrases'] = 'Interne Suchausdrücke';
+$lang['internalsearchwords']   = 'Interne Suchwörter';
+$lang['searchengines']         = 'Suchmaschinen';
+$lang['browsers']              = 'Browser';
+$lang['os']                    = 'Betriebssysteme';
+$lang['countries']             = 'Länder';
+$lang['resolution']            = 'Bildschirmauflösungen';
+$lang['dash_pageviews']        = '<strong>%d</strong> Seitenaufrufe';
+$lang['dash_sessions']         = '<strong>%d</strong> Besuche (Sitzung)';
+$lang['dash_visitors']         = '<strong>%d</strong> Individuelle Besucher';
+$lang['dash_users']            = '<strong>%d</strong> Eingeloggte Users';
+$lang['dash_logins']           = '<strong>%d</strong> User Logins';
+$lang['dash_registrations']    = '<strong>%s</strong> Neue Registrierungen';
+$lang['dash_newvisitors']      = '<strong>%.1f%%</strong> Neue Besucher';
+$lang['dash_mostpopular']      = 'Meistbesuchte Seiten';
+$lang['dash_topsearch']        = 'Top Suchausdrücke';
+$lang['graph_edits']           = 'Seiten bearbeitet';
+$lang['graph_creates']         = 'Seiten erstellt';
+$lang['graph_deletions']       = 'Seiten gelöscht';
+$lang['graph_views']           = 'Seitenaufrufe';
+$lang['graph_sessions']        = 'Besuche';
+$lang['graph_visitors']        = 'Besucher';
