8bfb342fe5fc51f85b148b14f67f38d4b6478f72
NicolasFriedli <nicolas@theologique.ch>
2013-12-07 21:40:04 +0100

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author NicolasFriedli <nicolas@theologique.ch>
+ */
+$lang['menu']                  = 'Statistiques d\'utilisation et d\'accès';
+$lang['more']                  = 'plus';
+$lang['prev']                  = 'page précédente';
+$lang['next']                  = 'page suivente';
+$lang['time_select']           = 'Sélectionner un intervalle de temps:';
+$lang['time_today']            = 'Aujourd\'hui';
+$lang['time_last1']            = 'Hier';
+$lang['time_last7']            = '7 derniers jours';
+$lang['time_last30']           = '30 derniers jours';
+$lang['time_go']               = 'Aller';
+$lang['dashboard']             = 'Tableau de bord';
+$lang['page']                  = 'Pages';
+$lang['referer']               = 'Liens entrants';
+$lang['newreferer']            = 'Nouveaux liens entrants';
+$lang['outlinks']              = 'Liens sortants';
+$lang['searchphrases']         = 'Recherches externes (expressions)';
+$lang['searchwords']           = 'Recherches externes (mots)';
+$lang['internalsearchphrases'] = 'Recherches internes (expressions)';
+$lang['internalsearchwords']   = 'Recherches internes (mots)';
+$lang['searchengines']         = 'Moteurs de recherche';
+$lang['browsers']              = 'Navigateurs';
+$lang['os']                    = 'Système d\'exploitation';
+$lang['countries']             = 'Pays';
+$lang['resolution']            = 'Taille de l\'écran';
+$lang['viewport']              = 'Viewport du navigateur';
+$lang['intro_page']            = 'Ces pages sont les plus vues du wiki durant l\'intervalle de temps sélectionné - votre contenu au top';
+$lang['intro_searchengines']   = 'Les moteurs de recheche suivants ont été utilisés pour trouver du contenu de votre wiki.';
+$lang['intro_internalsearchphrases'] = 'Motifs de recherche exacts utilisés dans la recherche interne de votre wiki.';
+$lang['intro_internalsearchwords'] = 'Mots les plus utilisés dans la recherche interne de votre wiki.';
+$lang['intro_browsers']        = 'Les navigateurs les plus populaires chez vos utilisateurs.';
+$lang['intro_os']              = 'Les systèmes d\'exploitation les plus populaires chez vos utilisateurs.';
+$lang['dash_pageviews']        = '<strong>%d</strong> pages vues';
+$lang['dash_sessions']         = '<strong>%d</strong> visites (sessions)';
+$lang['dash_visitors']         = '<strong>%d</strong> visiteurs uniques';
+$lang['dash_users']            = '<strong>%d</strong> visiteurs connectés';
+$lang['dash_registrations']    = '<strong>%s</strong> nouvelles inscriptions';
+$lang['dash_mostpopular']      = 'Pages les plus populaires';
+$lang['graph_sessions']        = 'Visites';
+$lang['graph_visitors']        = 'Visiteurs';
