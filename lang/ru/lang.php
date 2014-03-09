afc21d95ed878e12faa4c96fc3d84491e5f3dedc
Aleksandr Selivanov <alexgearbox@yandex.ru>
2014-03-09 17:30:34 +0100

    translation update

---
+++ lang/ru/lang.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
+ */
+$lang['more']                  = 'ещё';
+$lang['prev']                  = 'пред. стр.';
+$lang['next']                  = 'след. стр.';
+$lang['time_today']            = 'сегодня';
+$lang['time_last1']            = 'вчера';
+$lang['time_last7']            = 'последние 7 дней';
+$lang['time_last30']           = 'последние 30 дней';
+$lang['time_go']               = 'Перейти';
+$lang['dashboard']             = 'Панель';
+$lang['page']                  = 'Страницы';
+$lang['referer']               = 'Входящие ссылки';
+$lang['newreferer']            = 'Новые входящие ссылки';
+$lang['searchengines']         = 'Поисковыи системы';
+$lang['browsers']              = 'Браузеры';
+$lang['os']                    = 'Операционные системы';
+$lang['countries']             = 'Страны';
+$lang['resolution']            = 'Разрешения дисплеев';
+$lang['dash_visitors']         = 'Уникальных посетителей: <strong>%d</strong>';
+$lang['dash_mostpopular']      = 'Самые популярные страницы';
+$lang['graph_views']           = 'Просмотров страниц';
+$lang['graph_sessions']        = 'Посещений';
+$lang['graph_visitors']        = 'Посетителей';
