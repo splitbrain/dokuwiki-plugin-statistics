d59d4473ec1ba911926cd6d780fdb4c93841fb45
Rene <wllywlnt@yahoo.com>
2014-03-22 16:35:07 +0100

    translation update

---
+++ lang/nl/lang.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Rene <wllywlnt@yahoo.com>
+ */
+$lang['menu']                  = 'Toegang en gebruik statistieken';
+$lang['more']                  = 'meer';
+$lang['prev']                  = 'vorige pagina';
+$lang['next']                  = 'volgende pagina';
+$lang['time_select']           = 'Kies een periode:';
+$lang['time_today']            = 'Vandaag';
+$lang['time_last1']            = 'Gisteren';
+$lang['time_last7']            = 'Laatste 7 dagen';
+$lang['time_last30']           = 'Laatste 30 dagen';
+$lang['time_go']               = 'Voer uit';
+$lang['dashboard']             = 'Dashboard';
+$lang['page']                  = 'Pagina\'s';
+$lang['referer']               = 'Inkomende Links';
+$lang['newreferer']            = 'Niewe inkomende Links';
+$lang['outlinks']              = 'Uitgaande Links';
+$lang['searchphrases']         = 'Externe Zoek Uitdrukkingen';
+$lang['searchwords']           = 'Externe Zoek Woorden';
+$lang['internalsearchphrases'] = 'Interne Zoek Uitdrukkingen';
+$lang['internalsearchwords']   = 'Interne Zoek Woorden';
+$lang['searchengines']         = 'Zoekmachines';
+$lang['browsers']              = 'Browsers';
+$lang['os']                    = 'Besturingssysteem';
+$lang['countries']             = 'Landen';
+$lang['resolution']            = 'Scherm afmeting';
+$lang['viewport']              = 'Browser weergaveruimte';
+$lang['intro_dashboard']       = 'Deze pagina geeft een snel overzicht wat er in de wiki is gebeurd in de gekozen tijdperiode.<br /> Voor een gedetailleerde weergave en grafieken kies een onderwerp in de Inhoud Opgave';
+$lang['intro_page']            = 'Dit zijn de meest getoonde wiki pagina\'s in de geselecteerde periode - de top inhoud.';
+$lang['intro_referer']         = 'Van alle <strong>%d</strong> externe bezoeken, <strong>%d</strong> (<strong>%.1f%%</strong>) waren directe (of gebookmarkte) bezoeken, <strong>%d</strong> (<strong>%.1f%%</strong>) kwamen van zoekmachines en <strong>%d</strong> (<strong>%.1f%%</strong>)werden verwezen vanaf andere pagina\'s.<br />Deze andere pagina\'s zijn hieronder vermeld.';
+$lang['intro_newreferer']      = 'De volgende inkomende linken werden nooit eerder in de gekozen periode gelogd.';
+$lang['intro_outlinks']        = 'Dit zijn de meest gekozen links naar extrene sites in de wiki.';
+$lang['intro_searchengines']   = 'De volgende zoekmachines werden door gebruikers gebruikt om de wiki te vinden.';
+$lang['intro_searchphrases']   = 'Dit zijn de exacte uitdrukkingen die gebruikt werden om de wiki te vinden.';
+$lang['intro_searchwords']     = 'Dit zijn de meest gebruikte woorden die gebruikt werden om de wiki te vinden.';
+$lang['intro_internalsearchphrases'] = 'Dit zijn de exacte uitdrukkingen die gebruikt werden om te zoeken in de wiki.';
+$lang['intro_internalsearchwords'] = 'Dit zijn de meest gebruikte woorden die gebruikt werden om te zoeken in de wiki.';
+$lang['intro_browsers']        = 'Dit zijn de meest populaire browsers van de gebruikers.';
+$lang['intro_os']              = 'Dit zijn de meest populaire computer platvormen van de gebruikers.';
