7923e77d4f1a11b8343918f1dc31cdf76d351115
Wouter Wijsman <wwijsman@live.nl>
2015-01-05 15:10:14 +0100

    translation update

---
+++ lang/nl/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Wouter Wijsman <wwijsman@live.nl>
+ */
+$lang['db_server']             = 'Je database server (DNS naam of ip adres)';
+$lang['db_user']               = 'Je database gebruiker';
+$lang['db_password']           = 'Wachtwoord voor bovenstaande gebruiker';
+$lang['db_database']           = 'Te gebruiken database';
