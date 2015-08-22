4ac4f8d5705b958a500dda6f5844439181694e52
Padhie <develop@padhie.de>
2015-08-22 10:55:58 +0200

    translation update

---
+++ lang/de/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Padhie <develop@padhie.de>
+ */
+$lang['db_server']             = 'Dein Datenbankserver (DNS-name oder IP-Adresse)';
+$lang['db_user']               = 'Deine Datenbankbenutzer';
+$lang['db_database']           = 'Die Datenbank wird benutzt';
