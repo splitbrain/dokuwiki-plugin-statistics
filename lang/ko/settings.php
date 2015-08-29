c5262fa922c792a970852384231a9387e0c62660
Myeongjin <aranet100@gmail.com>
2015-08-29 10:40:05 +0200

    translation update

---
+++ lang/ko/settings.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+$lang['db_server']             = '데이터베이스 서버 (DNS 이름 또는 IP 주소)';
+$lang['db_user']               = '데이터베이스 사용자';
+$lang['db_password']           = '위 사용자의 비밀번호';
+$lang['db_database']           = '사용할 데이터베이스';
+$lang['db_prefix']             = '모든 테이블의 접두어';
+$lang['loggroups']             = '로그인해야하는 사용자 그룹의 쉼표로 구분된 목록. 모두이면 비워둘 것.';
