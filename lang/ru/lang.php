ec38ee8ed5bcbbabc5b5ea2363b077f7f3cc4120
Yuriy Skalko <yuriy.skalko@gmail.com>
2018-04-18 16:25:17 +0200

    translation update

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -3,27 +3,40 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
- * @author     Aleksandr Selivanov <alexgearbox@yandex.ru>
+ * @author Yuriy Skalko <yuriy.skalko@gmail.com>
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
  */
-$lang['more']             = 'ещё';
-$lang['prev']             = 'пред. стр.';
-$lang['next']             = 'след. стр.';
-$lang['time_today']       = 'сегодня';
-$lang['time_last1']       = 'вчера';
-$lang['time_last7']       = 'последние 7 дней';
-$lang['time_last30']      = 'последние 30 дней';
-$lang['time_go']          = 'Перейти';
-$lang['dashboard']        = 'Панель';
-$lang['page']             = 'Страницы';
-$lang['referer']          = 'Входящие ссылки';
-$lang['newreferer']       = 'Новые входящие ссылки';
-$lang['searchengines']    = 'Поисковыи системы';
-$lang['browsers']         = 'Браузеры';
-$lang['os']               = 'Операционные системы';
-$lang['countries']        = 'Страны';
-$lang['resolution']       = 'Разрешения дисплеев';
-$lang['dash_visitors']    = 'Уникальных посетителей: <strong>%d</strong>';
-$lang['dash_mostpopular'] = 'Самые популярные страницы';
-$lang['graph_views']      = 'Просмотров страниц';
-$lang['graph_sessions']   = 'Посещений';
-$lang['graph_visitors']   = 'Посетителей';
+$lang['more']                  = 'ещё';
+$lang['prev']                  = 'пред. стр.';
+$lang['next']                  = 'след. стр.';
+$lang['time_select']           = 'Выберите период:';
+$lang['time_today']            = 'сегодня';
+$lang['time_last1']            = 'вчера';
+$lang['time_last7']            = 'последние 7 дней';
+$lang['time_last30']           = 'последние 30 дней';
+$lang['time_go']               = 'Перейти';
+$lang['days']                  = 'Дни';
+$lang['weeks']                 = 'Недели';
+$lang['months']                = 'Месяцы';
+$lang['dashboard']             = 'Панель';
+$lang['page']                  = 'Страницы';
+$lang['images']                = 'Изображения';
+$lang['downloads']             = 'Загрузки';
+$lang['referer']               = 'Входящие ссылки';
+$lang['newreferer']            = 'Новые входящие ссылки';
+$lang['searchengines']         = 'Поисковыи системы';
+$lang['browsers']              = 'Браузеры';
+$lang['os']                    = 'Операционные системы';
+$lang['countries']             = 'Страны';
+$lang['resolution']            = 'Разрешения дисплеев';
+$lang['links']                 = '(Ссылки)';
+$lang['search']                = '(Поиск)';
+$lang['technology']            = '(Технология)';
+$lang['intro_browsers']        = 'Вот наиболее популярные браузеры, используемые вашими пользователями.';
+$lang['intro_os']              = 'Вот наиболее популярные платформы, используемые вашими пользователями.';
+$lang['dash_visitors']         = 'Уникальных посетителей: <strong>%d</strong>';
+$lang['dash_mostpopular']      = 'Самые популярные страницы';
+$lang['graph_views']           = 'Просмотров страниц';
+$lang['graph_sessions']        = 'Посещений';
+$lang['graph_visitors']        = 'Посетителей';
+$lang['graph_page_count']      = 'Страницы';
