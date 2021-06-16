cadaf208d2bb75552e1d16397ae1df51ac773dd5
Elvira <elviraparnell@gmail.com>
2021-06-16 08:55:30 +0200

    translation update

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,8 +1,14 @@
 <?php
 
-$lang['db_server']   = 'Your database server (DNS Name or IP Address)';
-$lang['db_user']     = 'Your database user';
-$lang['db_password'] = 'Password for above user';
-$lang['db_database'] = 'The database to use';
-$lang['db_prefix']   = 'A prefix for all tables';
-$lang['loggroups']   = 'Comma separated list of user groups that should be logged. Empty for all.';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Elvira <elviraparnell@gmail.com>
+ */
+$lang['db_server']             = 'Wijngracht 38';
+$lang['db_user']               = 'Elvira';
+$lang['db_password']           = 'For latest information you have to go to see world-wide-web and on internet I found this web site as a most excellent web page for latest updates.';
+$lang['db_database']           = 'For latest information you have to go to see world-wide-web and on internet I found this web site as 
+a most excellent web page for latest updates.';
+$lang['db_prefix']             = 'A prefix for all tables';
+$lang['loggroups']             = 'Elvira';
