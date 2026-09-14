ff671153a3c4d77490ffa4e6c261a439779d266e
E32k <Erik32konto@gmail.com>
2026-09-14 19:25:44 +0200

    Translation update (cs)

---
+++ lang/cs/lang.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author E32k <Erik32konto@gmail.com>
+ */
+$lang['menu']                  = 'Statistiky Přístupu a Využití';
+$lang['more']                  = 'více';
+$lang['prev']                  = 'předchozí stránka';
+$lang['next']                  = 'další stránka';
+$lang['time_select']           = 'Vyberte časový úsek:';
+$lang['time_today']            = 'Dnes';
+$lang['time_last1']            = 'Včera';
+$lang['time_last7']            = 'Posledních 7 Dnů';
+$lang['time_last30']           = 'Posledních 30 Dnů';
+$lang['days']                  = 'Dny';
+$lang['weeks']                 = 'Týdny';
+$lang['months']                = 'Měsíce';
+$lang['dashboard']             = 'Řídicí panel';
+$lang['pages']                 = 'Stránky';
+$lang['edits']                 = 'Změny';
+$lang['images']                = 'Obrázky';
+$lang['downloads']             = 'Stažené';
+$lang['campaigns']             = 'Kampaně';
+$lang['searchengines']         = 'Externí Vyhledávače';
+$lang['browsers']              = 'Prohlížeče';
+$lang['os']                    = 'Operační Systémy';
+$lang['countries']             = 'Země';
+$lang['resolution']            = 'Velikost Obrazovky';
+$lang['viewport']              = 'Zobrazovací Oblast Prohlížeče';
+$lang['seenusers']             = 'Zobrazení Uživetele';
+$lang['history']               = 'Historie Růstu';
+$lang['intro_outlinks']        = 'Toto jsou nejvíce navštěvované odkazy na externí stránky na vaší wiki.';
+$lang['intro_searchengines']   = 'Následující vyhledávače byly použity uživately aby našli vaší wiki.';
+$lang['intro_browsers']        = 'Tady jsou nejpopulárnější prohlížeče používané uživatelema.';
+$lang['intro_os']              = 'Tady jsou nejpopulárnější platformy používané uživatelema.';
+$lang['dash_pageviews']        = '<strong>%d</strong> Zobrazení Stránek';
+$lang['dash_visitors']         = '<strong>%d</strong> Nových Návštěvníků';
+$lang['dash_users']            = '<strong>%d</strong> Přihlášených Uživatelů';
+$lang['dash_logins']           = '<strong>%d</strong> Přihlášení Uživatelů';
+$lang['dash_registrations']    = '<strong>%s</strong> Nových Registrací';
+$lang['dash_mostpopular']      = 'Nejpopulárnější Stránky';
+$lang['graph_edits']           = 'Upravení Stránek';
+$lang['graph_creates']         = 'Vytvoření Stránek';
+$lang['graph_deletions']       = 'Smazání Stránek';
+$lang['graph_views']           = 'Zobrazení Stránek';
+$lang['graph_sessions']        = 'Návštěv';
+$lang['graph_visitors']        = 'Návštěvníků';
+$lang['graph_page_count']      = 'Stránek';
+$lang['graph_page_size']       = 'Velikost Stránek (MB)';
+$lang['graph_media_count']     = 'Media Položek';
+$lang['graph_media_size']      = 'Velikost Media Položek (MB)';
